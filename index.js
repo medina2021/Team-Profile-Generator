af44e10a9025bb48583e24e0b059d90e6774b1e2
Ever <evermedina2025@gmail.com>
2021-10-15 22:18:39 -0700

    created html render js for the team

--- index.js
+++ index.js
@@ -9,4 +9,9 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const outputPath = path.join(OUTPUT_DIR, "main.html");
+
+const render = require("./lib/htmlRender");
+
+const teamMembers= [];
+const idArray = [];
